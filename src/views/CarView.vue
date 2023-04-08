b81a4f5886da3f4ed6c59f4933e9af8440d0e876
3bd-ulrahman <abdulrahman198276301@gmail.com>
2023-04-08 17:57:34 +0200

    first commit

---
+++ src/views/CarView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>
+      car view
+    </h1>
+    <p>{{ car.name }}</p>
+    <p>{{ car.year }}</p>
+    <p>{{ car.price }}</p>
+    <button @click="router.push(`/cars/${car.id}/contact`)">Click for contact</button>
+    <RouterLink :to="`/cars/${car.id}/contact`">aaa</RouterLink>
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter, RouterView } from 'vue-router';
+import cars from '../data/cars.json';
+
+const route = useRoute();
+const router = useRouter();
+
+const car = cars.find(car => {
+  return car.id === parseInt(route.params.id)
+});
+</script>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>
+      Contact info
+    </h1>
+    <p>{{ car.contact.dealer }}</p>
+    <p>{{ car.contact.number }}</p>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, RouterView } from 'vue-router';
+import cars from '../data/cars.json';
+
+const route = useRoute();
+const car = cars.find(car => {
+  return car.id === parseInt(route.params.id)
+});
+</script>
